74d6589445f1c77bd7ef524187985e0727158891
WGS-94 <wilson-senx@hotmail.com>
2024-11-22 18:46:32 -0300

    :fire: Criando MapContainer para exibição do mapa

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import './App.css';
+import { useRef, useState } from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import osm from "./components/osm-providers.js";
+import "./App.css";
 
 function App() {
+  // -29.735549321048158, -51.16167775071418
+
+  const mapRef = useRef();
+
+  const [center, setCenter] = useState({ lat:-29.73555, lng: -51.16168});
+  const ZOOM_LEVEL = 9;
 
   return (
     <>
       <div>
-       Let´s go....
+        <MapContainer center={center} zoom={ZOOM_LEVEL} ref={mapRef}>
+          <TileLayer
+            url={osm.maptiler.url}
+            attribution={osm.maptiler.atribuition}
+          ></TileLayer>
+        </MapContainer>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
